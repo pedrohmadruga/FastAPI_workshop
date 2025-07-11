e29cbcee83cfef728a7c04ac44e93b8b3d638af4
Pedro Madruga <pedrohfonseca01@gmail.com>
2025-07-11 19:03:43 -0300

    Creating CRUD

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from typing import Optional
+from pydantic import BaseModel
+import uvicorn
 
 app = FastAPI()
 
@@ -21,6 +23,15 @@ def show(id: int):
     return {'data': id}
 
 @app.get('/blog/{id}/comments')
-def comments(id):
+def comments(id, limit=10):
     # fetch comments of blog with id = id
     return {'data': {'1', '2'}}
+
+class Blog(BaseModel):
+    title: str
+    body: str
+    published: Optional[bool] = None
+
+@app.post('/blog')
+def create_blog(blog:Blog):
+    return {'data': f"Blog is created with title as '{blog.title}'"}
